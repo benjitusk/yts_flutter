55ecd3f0ab58852beffa303ba1419461f20a50d2
BenjiTusk <benjitusk1@gmail.com>
2023-03-05 21:05:04 +0200

    Added placeholder Rabbi Page

---
+++ lib/widgets/pages/RabbiPage.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:yts_flutter/Classes/Author.dart';
+
+class RabbiPage extends StatelessWidget {
+  const RabbiPage({super.key, required this.rabbi});
+  final Author rabbi;
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
