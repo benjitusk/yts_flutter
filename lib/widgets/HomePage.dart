10206b6d05c35f495babab5d6e6bd0d164b7d7c9
BenjiTusk <benjitusk1@gmail.com>
2023-03-05 19:44:16 +0200

    Refactored home page components

--- lib/widgets/HomePage.dart
+++ lib/widgets/HomePage.dart
@@ -1,7 +1,9 @@
 // import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:yts_flutter/Classes/Author.dart';
+import 'package:yts_flutter/Classes/Shiur.dart';
 import 'package:yts_flutter/widgets/cards/HomeRabbiCard.dart';
+import 'package:yts_flutter/widgets/cards/HomeShiurCard.dart';
 import 'package:yts_flutter/widgets/helpers/TextDivider.dart';
 
 class HomePage extends StatefulWidget {
@@ -14,6 +16,7 @@ class HomePage extends StatefulWidget {
 class HomePageState extends State<HomePage> {
   // This will hold the list of news titles (list of strings)
   final List<Author> rebbeim = [];
+  final List<Shiur> recentShiurim = [];
   // Load rebbeim from Firebase when the page is loaded
   @override
   void initState() {
@@ -27,48 +30,78 @@ class HomePageState extends State<HomePage> {
     // that we're making production API calls
     print("====== PRODUCTION API CALLS ======");
     print(" 1) Fetching rebbeim from Firebase");
-    print("==================================");
     Author.loadAuthors().then((authors) {
       rebbeim.clear();
       // Sort by last name
       authors.sort((lhs, rhs) =>
           lhs.name.split(" ").last.compareTo(rhs.name.split(" ").last));
+      Author.authors = authors;
       setState(() {
         rebbeim.addAll(authors);
       });
     });
+    print(" 2) Fetching recent shiurim from Firebase");
+    Shiur.loadShiurim().then((shiurim) {
+      recentShiurim.clear();
+      setState(() {
+        recentShiurim.addAll(shiurim);
+      });
+    });
+    print("==================================");
   }
 
   @override
   Widget build(BuildContext context) {
-    // appBar: AppBar(
-
-    //   title: const Text('Home'),
-    // ),
-    return Column(
+    final sections = {
+      "Recent Shiurim": SizedBox(
+        height: 200,
+        child: Center(
+            child: ListView.separated(
+          scrollDirection: Axis.horizontal,
+          // Space the cards out a bit
+          padding: const EdgeInsets.all(8),
+          separatorBuilder: (context, index) => const SizedBox(width: 15),
+          itemCount: recentShiurim.length,
+          itemBuilder: (context, index) =>
+              HomeShiurCard(shiur: recentShiurim[index]),
+        )),
+      ),
+      "Rebbeim": SizedBox(
+        height: 200,
+        child: Center(
+            child: ListView.separated(
+          padding: const EdgeInsets.all(8.0),
+          scrollDirection: Axis.horizontal,
+          separatorBuilder: (context, index) => const SizedBox(width: 15),
+          itemCount: rebbeim.length,
+          itemBuilder: (context, index) => HomeRabbiCard(rabbi: rebbeim[index]),
+        )),
+      ),
+      "Categories": SizedBox(
+        height: 200,
+        child: Placeholder(),
+      ),
+      "Featured Pictures": SizedBox(
+        height: 200,
+        child: Placeholder(),
+      ),
+    };
+    return ListView(
       children: [
-        TextDivider(text: "Rebbeim"),
-        SizedBox(
-          height: 200,
-          child: Center(
-              child: ListView.separated(
-            scrollDirection: Axis.horizontal,
-            // Space the cards out a bit
-            padding: const EdgeInsets.all(8),
-            separatorBuilder: (context, index) => const SizedBox(width: 15),
-            itemCount: rebbeim.length,
-            itemBuilder: (context, index) =>
-                HomeRabbiCard(rabbi: rebbeim[index]),
-          )),
-        ),
-        Divider()
+        ...sections
+            .map((title, widget) => MapEntry(
+                title,
+                Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: TextDivider(text: title),
+                    ),
+                    widget,
+                  ],
+                )))
+            .values
       ],
     );
-    // Center(
-    //     child: ListView(
-    //   scrollDirection: Axis.horizontal,
-    //   children:
-    //       rebbeim.map((Author rabbi) => HomeRabbiCard(rabbi: rabbi)).toList(),
-    // )),
   }
 }
