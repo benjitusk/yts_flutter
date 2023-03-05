24697209bc93e8242ddde3c3b91eb2d88f5b2531
BenjiTusk <benjitusk1@gmail.com>
2023-03-05 19:43:47 +0200

    removed padding

--- lib/widgets/helpers/TextDivider.dart
+++ lib/widgets/helpers/TextDivider.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
 
 class TextDivider extends StatelessWidget {
   const TextDivider({super.key, required this.text});
@@ -9,20 +7,17 @@ class TextDivider extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.symmetric(horizontal: 8),
-      child: Row(
-        children: [
-          Text(text, style: Theme.of(context).textTheme.titleLarge),
-          SizedBox(width: 8),
-          Expanded(
-            child: Divider(
-              color: dividerColor,
-              thickness: 0.51,
-            ),
+    return Row(
+      children: [
+        Text(text, style: Theme.of(context).textTheme.titleLarge),
+        SizedBox(width: 8),
+        Expanded(
+          child: Divider(
+            color: dividerColor,
+            thickness: 0.51,
           ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
