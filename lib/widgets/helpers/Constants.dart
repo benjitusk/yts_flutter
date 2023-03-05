1e0f2b7fa43051bfa12b81f77386db37651cf4a8
BenjiTusk <benjitusk1@gmail.com>
2023-03-05 20:42:52 +0200

    Created UI constants file

---
+++ lib/widgets/helpers/Constants.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class UI {
+  static final cardGradient = LinearGradient(
+    begin: Alignment.bottomLeft,
+    end: Alignment.centerLeft,
+    colors: [
+      Color.fromARGB(255, 24, 33, 48),
+      Color.fromARGB(255, 74, 91, 130),
+    ],
+  );
+}
