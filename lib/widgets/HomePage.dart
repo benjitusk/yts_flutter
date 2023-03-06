9f3ac256b914c5958a57d751c8e4a4a0f5ad6b1d
BenjiTusk <benjitusk1@gmail.com>
2023-03-06 15:36:07 +0200

    Implemented Slideshow

--- lib/widgets/HomePage.dart
+++ lib/widgets/HomePage.dart
@@ -1,7 +1,11 @@
 // import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:yts_flutter/Classes/Author.dart';
 import 'package:yts_flutter/Classes/Shiur.dart';
+import 'package:yts_flutter/widgets/Slideshow.dart';
 import 'package:yts_flutter/widgets/cards/HomeRabbiCard.dart';
 import 'package:yts_flutter/widgets/cards/HomeShiurCard.dart';
 import 'package:yts_flutter/widgets/helpers/TextDivider.dart';
@@ -17,15 +21,16 @@ class HomePageState extends State<HomePage> {
   // This will hold the list of news titles (list of strings)
   final List<Author> rebbeim = [];
   final List<Shiur> recentShiurim = [];
+  final List<String> featuredImageURLs = [];
   // Load rebbeim from Firebase when the page is loaded
   @override
   void initState() {
     super.initState();
-    loadRebbeim();
+    loadData();
   }
 
   // This will fetch the news titles from Firebase
-  void loadRebbeim() async {
+  void loadData() async {
     // Log a clearly identifiable message
     // that we're making production API calls
     print("====== PRODUCTION API CALLS ======");
@@ -45,6 +50,25 @@ class HomePageState extends State<HomePage> {
       recentShiurim.clear();
       setState(() {
         recentShiurim.addAll(shiurim);
+      });
+    });
+    print(" 3) Fetching featured pictures from Firebase");
+    FirebaseFirestore.instance
+        .collection("slideshowImages")
+        .orderBy("uploaded", descending: true)
+        .limit(15)
+        .get()
+        .then((snapshot) async {
+      Future.wait(snapshot.docs.map((doc) async {
+        final String url = await FirebaseStorage.instance
+            .ref("slideshow/${doc.data()['image_name']}")
+            .getDownloadURL();
+        return url;
+      })).then((urls) {
+        featuredImageURLs.clear();
+        setState(() {
+          featuredImageURLs.addAll(urls);
+        });
       });
     });
     print("==================================");
@@ -81,13 +105,13 @@ class HomePageState extends State<HomePage> {
         height: 200,
         child: Placeholder(),
       ),
-      "Featured Pictures": SizedBox(
-        height: 200,
-        child: Placeholder(),
+      "Featured Pictures": ImageCarousel(
+        urls: featuredImageURLs,
       ),
     };
     return ListView(
       children: [
+        TextButton(onPressed: loadData, child: Text("Reload")),
         ...sections
             .map((title, widget) => MapEntry(
                 title,
---
+++ lib/widgets/Slideshow.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:cached_network_image/cached_network_image.dart';
+
+class ImageCarousel extends StatelessWidget {
+  const ImageCarousel({super.key, required this.urls});
+  final List<String> urls;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      child: CarouselSlider.builder(
+          itemCount: urls.length,
+          itemBuilder: (context, index, realIndex) {
+            return SizedBox(
+              width: MediaQuery.of(context).size.width,
+              child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(8),
+                    child: urls.isEmpty
+                        ? Center(child: CircularProgressIndicator())
+                        : CachedNetworkImage(
+                            fit: BoxFit.cover,
+                            imageUrl: urls[index],
+                            progressIndicatorBuilder:
+                                (context, url, downloadProgress) => Center(
+                                    child: CircularProgressIndicator(
+                                        value: downloadProgress.progress)),
+                            errorWidget: (context, url, error) =>
+                                Icon(Icons.error),
+                          ),
+                  )),
+            );
+          },
+          options: CarouselOptions(
+              // disableCenter: true,
+              autoPlay: !true,
+              autoPlayInterval: Duration(seconds: 10),
+              autoPlayAnimationDuration: Duration(milliseconds: 800),
+              autoPlayCurve: Curves.fastOutSlowIn,
+              enlargeCenterPage: true,
+              scrollDirection: Axis.horizontal,
+              // padEnds: false,
+              viewportFraction: 1)),
+      // child: CarouselSlider(
+      //     items: [
+      //       for (final url in urls)
+      //         Container(
+      //           child: Center(
+      //               child: Image.network(url,
+      //                   filterQuality: FilterQuality.low,
+      //                   cacheHeight: 200,
+      //                   cacheWidth: 200,
+      //                   loadingBuilder: (context, child, loadingProgress) {
+      //             if (loadingProgress == null) return child;
+      //             return Center(
+      //                 child: CircularProgressIndicator(
+      //                     value: loadingProgress.expectedTotalBytes != null
+      //                         ? loadingProgress.cumulativeBytesLoaded /
+      //                             loadingProgress.expectedTotalBytes!
+      //                         : null));
+      //           })
+      //               // Text(
+      //               //     "Loading: ${(loadingProgress?.cumulativeBytesLoaded ?? 1) / (loadingProgress?.expectedTotalBytes ?? 1) * 100}%")),
+      //               ),
+      //         )
+      //     ],
+      //     options: CarouselOptions(
+      //       // height: 200,
+      //       autoPlay: !true,
+      //       autoPlayInterval: Duration(seconds: 3),
+      //       autoPlayAnimationDuration: Duration(milliseconds: 800),
+      //       autoPlayCurve: Curves.fastOutSlowIn,
+      //       enlargeCenterPage: true,
+      //       scrollDirection: Axis.horizontal,
+      //     )),
+    );
+  }
+}
