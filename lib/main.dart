94d6933e6aec59375322994eb9d1f704ad5f8ac3
BenjiTusk <benjitusk1@gmail.com>
2023-03-06 18:04:42 +0200

    updated theme

--- lib/main.dart
+++ lib/main.dart
@@ -21,6 +21,8 @@ void main() async {
   runApp(const MyApp());
 }
 
+final theme = ThemeData();
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -29,9 +31,27 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+      theme: theme.copyWith(
+          colorScheme: theme.colorScheme.copyWith(
+        primary: const Color(0xFF526B98),
+        secondary: const Color(0xFFFCC730),
+      )),
+
+      // ThemeData(
+      //   colorScheme: ColorScheme.,
+      //     primarySwatch: MaterialColor(0xFF526B98, {
+      //   50: Color(0xFFE8EAF6),
+      //   100: Color(0xFFC5CAE9),
+      //   200: Color(0xFF9FA8DA),
+      //   300: Color(0xFF7986CB),
+      //   400: Color(0xFF5C6BC0),
+      //   500: Color(0xFF3F51B5),
+      //   600: Color(0xFF3949AB),
+      //   700: Color(0xFF303F9F),
+      //   800: Color(0xFF283593),
+      //   900: Color(0xFF1A237E),
+      // })
+      // ),
       home: DefaultTabController(
         length: 1,
         child: Scaffold(
