cee59ff7f318b2f5d48fcb0d8ffa38a1a7de92a4
BenjiTusk <benjitusk1@gmail.com>
2023-03-13 22:47:36 +0200

    cleaned up main.dart (removed old color scheme)

--- lib/main.dart
+++ lib/main.dart
@@ -36,22 +36,6 @@ class MyApp extends StatelessWidget {
         primary: const Color(0xFF526B98),
         secondary: const Color(0xFFFCC730),
       )),
-
-      // ThemeData(
-      //   colorScheme: ColorScheme.,
-      //     primarySwatch: MaterialColor(0xFF526B98, {
-      //   50: Color(0xFFE8EAF6),
-      //   100: Color(0xFFC5CAE9),
-      //   200: Color(0xFF9FA8DA),
-      //   300: Color(0xFF7986CB),
-      //   400: Color(0xFF5C6BC0),
-      //   500: Color(0xFF3F51B5),
-      //   600: Color(0xFF3949AB),
-      //   700: Color(0xFF303F9F),
-      //   800: Color(0xFF283593),
-      //   900: Color(0xFF1A237E),
-      // })
-      // ),
       home: DefaultTabController(
         length: 1,
         child: Scaffold(
