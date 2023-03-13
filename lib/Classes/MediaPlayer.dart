8826a8e4a9c17b455cd6aa3472e5be7839f44033
BenjiTusk <benjitusk1@gmail.com>
2023-03-13 22:47:12 +0200

    settled on implementation for Audio Player

--- lib/Classes/MediaPlayer.dart
+++ lib/Classes/MediaPlayer.dart
@@ -1,23 +1,19 @@
 import 'package:assets_audio_player/assets_audio_player.dart';
-import 'package:firebase_storage/firebase_storage.dart';
 import 'package:yts_flutter/Classes/Streamable.dart';
-
-const HASH = '039e62c8b146a76f19a2f42cc1e48161b0d097c21037aab51e448dfa41aa46fd';
 
 class MediaPlayer {
   MediaPlayer._() {}
   static final instance = MediaPlayer._();
   static shared() => instance;
-  final player = AssetsAudioPlayer.newPlayer();
+  final player = AssetsAudioPlayer();
 
   void play(Streamable content) async {
-    final path = content.playbackUrl;
-    try {
-      await player.open(Audio.network(path),
-          showNotification: true, notificationSettings: NotificationSettings());
-      player.play();
-    } catch (e) {
-      print("Couldn't load media from ${path}");
-    }
+    await player.open(
+      Audio.network(content.playbackUrl),
+      autoStart: true,
+      showNotification: true,
+      playInBackground: PlayInBackground.enabled,
+    );
+    player.play();
   }
 }
--- lib/Classes/Streamable.dart
+++ lib/Classes/Streamable.dart
@@ -1,3 +1,7 @@
+import 'package:yts_flutter/Classes/Author.dart';
+
 abstract class Streamable {
   late String playbackUrl;
+  late BasicAuthor author;
+  late String title;
 }
