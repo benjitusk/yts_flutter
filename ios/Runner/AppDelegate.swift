5dde2c63b00bcf645c36b4e5ba3249c0ebb53ce3
BenjiTusk <benjitusk1@gmail.com>
2023-03-13 22:46:22 +0200

    implemented production mode AppCheck (iOS)

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -8,11 +8,23 @@ import Firebase
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-//    #if DEBUG
+#if DEBUG
     let providerFactory = AppCheckDebugProviderFactory()
+#else
+    let providerFactory = YTSAppCheckProviderFactory()
+#endif
     AppCheck.setAppCheckProviderFactory(providerFactory)
-//    #endif
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+}
+
+class YTSAppCheckProviderFactory: NSObject, AppCheckProviderFactory {
+  func createProvider(with app: FirebaseApp) -> AppCheckProvider? {
+    if #available(iOS 14.0, *) {
+      return AppAttestProvider(app: app)
+    } else {
+      return DeviceCheckProvider(app: app)
+    }
   }
 }
