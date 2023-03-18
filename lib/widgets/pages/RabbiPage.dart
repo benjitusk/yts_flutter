b9db9c8fb172fa5ddbde57cf17a645ec801f295d
BenjiTusk <benjitusk1@gmail.com>
2023-03-19 00:48:01 +0200

    Implemented Scaffold Rabbi page

--- lib/widgets/pages/RabbiPage.dart
+++ lib/widgets/pages/RabbiPage.dart
@@ -1,6 +1,9 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 import 'package:yts_flutter/Classes/Author.dart';
+import 'package:yts_flutter/widgets/helpers/ImageCircle.dart';
 
 class RabbiPage extends StatelessWidget {
   const RabbiPage({super.key, required this.rabbi});
@@ -8,6 +11,20 @@ class RabbiPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(rabbi.name),
+          actions: [
+            Padding(
+              padding: const EdgeInsets.only(right: 8),
+              child: CircleAvatar(
+                minRadius: 25,
+                backgroundImage:
+                    CachedNetworkImageProvider(rabbi.profilePictureURL),
+              ),
+            )
+          ],
+        ),
+        body: Placeholder());
   }
 }
