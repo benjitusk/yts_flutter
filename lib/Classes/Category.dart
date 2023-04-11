07d36c620523e6095a99907331493373255e298b
BenjiTusk <benjitusk1@gmail.com>
2023-04-11 01:30:01 -0400

    refactored all backend logic

--- lib/Classes/Category.dart
+++ lib/Classes/Category.dart
@@ -1,9 +1,11 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_storage/firebase_storage.dart';
+import 'package:yts_flutter/classes/misc_types.dart';
 
 const MISC_ID = "NKwXl5QXmOe6rlQ9J3kW";
 
 class Category {
+  final FirebaseID id;
   final String hiddenName;
   final String? imageURL;
   final String displayName;
@@ -11,7 +13,8 @@ class Category {
   Category(
       {required this.hiddenName,
       required this.imageURL,
-      required this.displayName});
+      required this.displayName,
+      required this.id});
 
   static Future<List<Category>> loadCategories() async {
     final snapshot = await FirebaseFirestore.instance
@@ -19,6 +22,7 @@ class Category {
         .where("isParent", isEqualTo: true)
         .get();
     final categoryFutures = snapshot.docs.map((doc) async {
+      final id = doc.id;
       final String? url = doc.id == MISC_ID
           ? null
           : await FirebaseStorage.instance
@@ -32,6 +36,7 @@ class Category {
         hiddenName: doc.data()['name'],
         imageURL: url,
         displayName: doc.data()['displayName'],
+        id: id,
       );
     });
     return Future.wait(categoryFutures);
