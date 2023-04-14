6799156f54e6392cf9e0858d27bc7c07b6fa6b8b
BenjiTusk <benjitusk1@gmail.com>
2023-04-14 16:34:36 -0400

    Nearly finished Category page

--- lib/widgets/pages/category_page.dart
+++ lib/widgets/pages/category_page.dart
@@ -1,8 +1,11 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:yts_flutter/classes/author.dart';
 import 'package:yts_flutter/classes/category.dart';
 import 'package:yts_flutter/classes/shiur.dart';
+import 'package:yts_flutter/extensions/Duration.dart';
 import 'package:yts_flutter/services/backend_manager.dart';
-import 'package:yts_flutter/widgets/cards/standard_shiur_card.dart';
 
 class CategoryPage extends StatefulWidget {
   const CategoryPage({super.key, required this.category});
@@ -15,6 +18,7 @@ class CategoryPage extends StatefulWidget {
 class _CategoryPageState extends State<CategoryPage> {
   // State variables:
   List<Shiur>? content = null;
+  List<Category>? subCategories = null;
 
   @override
   void initState() {
@@ -45,17 +49,77 @@ class _CategoryPageState extends State<CategoryPage> {
 
   @override
   Widget build(BuildContext context) {
+    final numberOfSubCategories = subCategories?.length ?? 0;
     return Scaffold(
         appBar: AppBar(
           title: Text(widget.category.displayName),
         ),
-        body: content != null
+        body: ((content?.isNotEmpty ?? false) ||
+                (subCategories?.isNotEmpty ?? false))
             ? ListView.separated(
                 itemBuilder: (context, index) {
-                  return ShiurCard(shiur: content![index]);
+                  if (subCategories != null && index < numberOfSubCategories) {
+                    return ListTile(
+                      leading: Icon(Icons.folder),
+                      title: Text(subCategories![index].displayName),
+                      onTap: () => Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => CategoryPage(
+                                  category: subCategories![index]))),
+                    );
+                  }
+                  if (content != null)
+                    return ContentDataTable(
+                        shiur: content![index - numberOfSubCategories]);
+
+                  return const Center(child: CircularProgressIndicator());
                 },
-                separatorBuilder: (context, index) => const Divider(),
-                itemCount: content!.length)
-            : const Center(child: CircularProgressIndicator()));
+                separatorBuilder: (context, index) => const Divider(
+                      height: 1,
+                    ),
+                itemCount: (content?.length ?? 0) + numberOfSubCategories)
+            :
+            // We checked, and there is no content in this category.
+            /// TODO: Show a nice message.
+            const Center(child: Text('No content in this category')));
+  }
+}
+
+class ContentDataTable extends StatelessWidget {
+  const ContentDataTable({super.key, required this.shiur});
+  final _imageSize = 50.0;
+  final Shiur shiur;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+        leading: (shiur.author is Author)
+            ? CachedNetworkImage(
+                imageUrl: (shiur.author as Author).profilePictureURL,
+                imageBuilder: (context, imageProvider) => Container(
+                  width: _imageSize,
+                  height: _imageSize,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    image: DecorationImage(
+                      image: imageProvider,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+                placeholder: (context, url) =>
+                    const CircularProgressIndicator(),
+                errorWidget: (context, url, error) => const Icon(Icons.error),
+              )
+            : const Icon(Icons.mic),
+        title: Text(shiur.title),
+        subtitle: Text(
+          DateFormat.yMMMd().format(shiur.date),
+        ),
+        // isThreeLine: true,
+
+        trailing: Text(shiur.duration.toHoursMinutesSeconds()),
+        onTap: () => null);
   }
 }
