bc705735263c1a72a98b4a196fb47581f01533a5
BenjiTusk <benjitusk1@gmail.com>
2023-06-08 19:42:41 +0300

    cleaned up category page

--- lib/widgets/pages/category_page.dart
+++ lib/widgets/pages/category_page.dart
@@ -13,52 +13,51 @@ class CategoryPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListenableBuilder(
-      listenable: model,
-      builder: (context, _) => Scaffold(
+    return Scaffold(
         appBar: AppBar(
           title: Text(category.displayName),
         ),
-        body: (() {
-          if (model.isLoading) {
-            return const Center(child: CircularProgressIndicator());
-          }
-          if (model.isEmpty)
-            return const Center(child: Text('No content found.'));
+        body: ListenableBuilder(
+          listenable: model,
+          builder: (context, _) {
+            if (model.isLoading) {
+              return const Center(child: CircularProgressIndicator());
+            }
+            if (model.isEmpty)
+              return const Center(child: Text('No content found.'));
 
-          if (model.error != null)
-            showDialog(
-                context: context,
-                builder: (context) => AlertDialog(
-                      title: const Text('Error'),
-                      content: Text(
-                          'Error: ${model.error.toString()}. Please try again.'),
-                      actions: [
-                        TextButton(
-                          child: const Text('OK'),
-                          onPressed: () => Navigator.of(context).pop(),
-                        ),
-                      ],
-                    ));
-          return ListView.separated(
-              itemBuilder: (context, index) {
-                if (index < model.subCategories!.length) {
-                  return ListTile(
-                    leading: Icon(Icons.folder),
-                    title: Text(model.subCategories![index].displayName),
-                    onTap: () => Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => CategoryPage(
-                                category: model.subCategories![index]))),
-                  );
-                }
-                return ContentTableRow(shiur: model.content![index]);
-              },
-              separatorBuilder: (context, index) => const Divider(),
-              itemCount: model.subCategories!.length + model.content!.length);
-        })(),
-      ),
-    );
+            if (model.error != null)
+              showDialog(
+                  context: context,
+                  builder: (context) => AlertDialog(
+                        title: const Text('Error'),
+                        content: Text(
+                            'Error: ${model.error.toString()}. Please try again.'),
+                        actions: [
+                          TextButton(
+                            child: const Text('OK'),
+                            onPressed: () => Navigator.of(context).pop(),
+                          ),
+                        ],
+                      ));
+            return ListView.separated(
+                itemBuilder: (context, index) {
+                  if (index < model.subCategories!.length) {
+                    return ListTile(
+                      leading: Icon(Icons.folder),
+                      title: Text(model.subCategories![index].displayName),
+                      onTap: () => Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => CategoryPage(
+                                  category: model.subCategories![index]))),
+                    );
+                  }
+                  return ContentTableRow(shiur: model.content![index]);
+                },
+                separatorBuilder: (context, index) => const Divider(),
+                itemCount: model.subCategories!.length + model.content!.length);
+          },
+        ));
   }
 }
