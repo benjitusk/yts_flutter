8c4ce9a3cc7f11fed1abb6c40c1d4f5d765c0bae
BenjiTusk <benjitusk1@gmail.com>
2023-10-22 03:18:03 +0300

    Added version number, fixed dev mode switch

--- lib/widgets/screens/HomeScreen/settings_screen.dart
+++ lib/widgets/screens/HomeScreen/settings_screen.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
+import 'package:package_info_plus/package_info_plus.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:yts_flutter/utils.dart';
 
@@ -11,109 +12,189 @@ class SettingsScreen extends StatefulWidget {
 }
 
 class _SettingsScreenState extends State<SettingsScreen> {
+  final bloc = SettingsBloc();
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      padding: const EdgeInsets.all(8.0),
-      children: [
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Text("Settings",
-              style: Theme.of(context).textTheme.headlineMedium),
-        ),
-        // Spacer
-        Card(
-          clipBehavior: Clip.hardEdge,
-          // Link to notifications settings page
-          child: ListTile(
-              leading: const Icon(Icons.notifications_active),
-              trailing: const Icon(Icons.chevron_right),
-              title: const Text("Notifications"),
-              subtitle: const Text("Manage notifications from YTS"),
-              onTap: () {
-                FirebaseMessaging.instance.requestPermission();
-                Navigator.of(context).push(MaterialPageRoute(
-                    builder: (context) => Scaffold(
-                          appBar: AppBar(title: Text("Notifications")),
-                          body: ListView(
-                            padding: const EdgeInsets.all(8.0),
-                            children: [
-                              Card(
-                                child: ListTile(
-                                  leading: const Icon(Icons.notifications),
-                                  title: const Text("Notifications"),
-                                  subtitle: const Text(
-                                      "Manage notifications from YTS"),
-                                  trailing: Switch(
-                                    // This bool value toggles the switch.
-                                    value: false,
-                                    activeColor: Colors.red,
-                                    onChanged: null,
-                                  ),
+    return ListenableBuilder(
+        listenable: bloc,
+        builder: (context, snapshot) {
+          return ListView(
+            padding: const EdgeInsets.all(8.0),
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text("Settings",
+                    style: Theme.of(context).textTheme.headlineMedium),
+              ),
+              // Spacer
+              Card(
+                clipBehavior: Clip.hardEdge,
+                // Link to notifications settings page
+                child: ListTile(
+                    leading: const Icon(Icons.notifications_active),
+                    trailing: const Icon(Icons.chevron_right),
+                    title: const Text("Notifications"),
+                    subtitle: const Text("Manage notifications from YTS"),
+                    onTap: () {
+                      FirebaseMessaging.instance.requestPermission();
+                      Navigator.of(context).push(MaterialPageRoute(
+                          builder: (context) => Scaffold(
+                                appBar: AppBar(title: Text("Notifications")),
+                                body: ListView(
+                                  padding: const EdgeInsets.all(8.0),
+                                  children: [
+                                    Card(
+                                      child: ListTile(
+                                        leading:
+                                            const Icon(Icons.notifications),
+                                        title: const Text("Notifications"),
+                                        subtitle: const Text(
+                                            "Manage notifications from YTS"),
+                                        trailing: Switch(
+                                          // This bool value toggles the switch.
+                                          value: false,
+                                          activeColor: Colors.red,
+                                          onChanged: null,
+                                        ),
+                                      ),
+                                    ),
+                                  ],
                                 ),
-                              ),
-                            ],
-                          ),
-                        )));
-              }),
-        ),
-        Card(
-            clipBehavior: Clip.hardEdge,
-            child: ListTile(
-                leading: const Icon(Icons.cloud_upload_rounded),
-                title: const Text("Upload a shiur"),
-                subtitle:
-                    const Text("Submit a shiur to be uploaded to the app"),
-                trailing: const Icon(Icons.chevron_right),
-                onTap: () => null)),
-        Card(
-            clipBehavior: Clip.hardEdge,
-            child: ListTile(
-                leading: const Icon(Icons.info_outline),
-                title: const Text("About"),
-                trailing: const Icon(Icons.chevron_right),
-                onTap: () => Navigator.of(context).push(MaterialPageRoute(
-                    builder: (context) => Scaffold(
-                          appBar: AppBar(title: Text("About")),
-                          body: _AboutPage(),
-                        )))))
-      ],
-    );
+                              )));
+                    }),
+              ),
+              Card(
+                  clipBehavior: Clip.hardEdge,
+                  child: ListTile(
+                      leading: const Icon(Icons.cloud_upload_rounded),
+                      title: const Text("Upload a shiur"),
+                      subtitle: const Text(
+                          "Submit a shiur to be uploaded to the app"),
+                      trailing: const Icon(Icons.chevron_right),
+                      onTap: () => null)),
+              Card(
+                  clipBehavior: Clip.hardEdge,
+                  child: ListTile(
+                      leading: const Icon(Icons.card_giftcard),
+                      title: const Text("Sponsor learning at Yeshiva"),
+                      subtitle: const Text(
+                          "Sponsor the learning in Yeshiva and on the app"),
+                      trailing: const Icon(Icons.chevron_right),
+                      onTap: onSponsorshipPromptClick)),
+              Card(
+                  clipBehavior: Clip.hardEdge,
+                  child: ListTile(
+                      leading: const Icon(Icons.info_outline),
+                      title: const Text("About"),
+                      trailing: const Icon(Icons.chevron_right),
+                      onTap: () => Navigator.of(context).push(MaterialPageRoute(
+                          builder: (context) => Scaffold(
+                                appBar: AppBar(title: Text("About")),
+                                body: _AboutPage(
+                                  bloc: bloc,
+                                ),
+                              )))))
+            ],
+          );
+        });
   }
 }
 
 class _AboutPage extends StatelessWidget {
-  _AboutPage();
-  final bloc = AboutBloc();
+  final SettingsBloc bloc;
+  const _AboutPage({required this.bloc});
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      padding: const EdgeInsets.all(8.0),
-      children: [
-        Card(
-            child: ListTile(
-          leading:
-              // ColorFiltered(
-              //   colorFilter: ColorFilter.mode(
-              //       Theme.of(context).colorScheme.onBackground, BlendMode.srcATop),
-              //   child:
-              ShragaLogo(dark: isDarkTheme(context)),
-          // ),
-          title: Text("Yeshivat Torat Shraga"),
-          subtitle:
-              Text("App developed by Benji Tusk (${bloc.isDevModeEnabled})"),
-          onTap: () => bloc.setDevMode(!bloc.isDevModeEnabled),
-        )),
-      ],
-    );
+    return ListenableBuilder(
+        listenable: bloc,
+        builder: (context, snapshot) {
+          return Padding(
+            padding: const EdgeInsets.only(left: 8.0, right: 8.0, top: 16.0),
+            child: Column(
+              // As big as the screen
+              mainAxisSize: MainAxisSize.max,
+              children: [
+                Card(
+                    clipBehavior: Clip.hardEdge,
+                    child: ListTile(
+                      leading:
+                          // ColorFiltered(
+                          //   colorFilter: ColorFilter.mode(
+                          //       Theme.of(context).colorScheme.onBackground, BlendMode.srcATop),
+                          //   child:
+                          ShragaLogo(dark: isDarkTheme(context)),
+                      // ),
+                      title: Text("Yeshivat Torat Shraga"),
+                      subtitle: Text("Developed by Benji Tusk"),
+                      onLongPress: () =>
+                          bloc.setDevMode(!bloc.isDevModeEnabled),
+                    )),
+                Spacer(),
+                // Version number
+                Row(
+                  children: [
+                    Text(getCopyrightText(),
+                        style: Theme.of(context).textTheme.bodyMedium?.copyWith(
+                            color: Theme.of(context)
+                                .colorScheme
+                                .onBackground
+                                .withOpacity(0.5))),
+                    Spacer(),
+                    FutureBuilder<PackageInfo>(
+                      future: PackageInfo.fromPlatform(),
+                      builder: (context, snapshot) {
+                        switch (snapshot.connectionState) {
+                          case ConnectionState.done:
+                            return Align(
+                              alignment: Alignment.bottomCenter,
+                              child: bloc.isDevModeEnabled
+                                  ? GradientText(
+                                      'Version: ${snapshot.data?.version} (${snapshot.data?.buildNumber}) DEV',
+                                      gradient: LinearGradient(colors: [
+                                        Colors.blue.shade400,
+                                        Colors.blue.shade900,
+                                      ]))
+                                  : Text(
+                                      'Version: ${snapshot.data?.version} (${snapshot.data?.buildNumber})',
+                                      style: Theme.of(context)
+                                          .textTheme
+                                          .bodyMedium
+                                          ?.copyWith(
+                                              color: Theme.of(context)
+                                                  .colorScheme
+                                                  .onBackground
+                                                  .withOpacity(0.5)),
+                                    ),
+                            );
+                          default:
+                            return const SizedBox();
+                        }
+                      },
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          );
+        });
+  }
+
+  String getCopyrightText() {
+    final year = DateTime.now().year;
+    String copyRightText = "© Benji Tusk 2023";
+    if (year > 2023) {
+      return copyRightText + "-$year";
+    } else {
+      return copyRightText;
+    }
   }
 }
 
-class AboutBloc extends ChangeNotifier {
+class SettingsBloc extends ChangeNotifier {
   bool _isDevModeEnabled = false;
   bool get isDevModeEnabled => _isDevModeEnabled;
 
-  AboutBloc() {
+  SettingsBloc() {
     SharedPreferences.getInstance().then((prefs) {
       _isDevModeEnabled = prefs.getBool('devMode') ?? false;
       notifyListeners();
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -19,6 +19,7 @@ import firebase_core
 import firebase_messaging
 import firebase_storage
 import just_audio
+import package_info_plus
 import path_provider_foundation
 import shared_preferences_foundation
 import sqflite
@@ -39,6 +40,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseMessagingPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseMessagingPlugin"))
   FLTFirebaseStoragePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseStoragePlugin"))
   JustAudioPlugin.register(with: registry.registrar(forPlugin: "JustAudioPlugin"))
+  FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
